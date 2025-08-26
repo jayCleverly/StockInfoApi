e74f92dcc5839fd79fb69004c45cb9d17d51ad60
Jay <jaydcleverly@gmail.com>
2025-08-26 17:48:15 +0100

    Initialized spring boot

---
+++ src/main/java/com/github/jaycleverly/stock_info/StockInfoApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaycleverly.stock_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jaycleverly/stock_info/StockInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaycleverly.stock_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
